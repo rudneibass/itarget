3a9b4433088fbdcb0c5e3d55a5c64e235c018c38
Rudnei Nascimento <rudneixaviernascimento@gmail.com>
2023-12-12 08:46:05 -0300

    README.md

--- backend/database/migrations/2023_12_08_220941_registrations_create.php
+++ backend/database/migrations/2023_12_08_220941_registrations_create.php
@@ -2,7 +2,9 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
+use Faker\Factory as Faker;
 
 class RegistrationsCreate extends Migration
 {
@@ -13,6 +15,8 @@ class RegistrationsCreate extends Migration
      */
     public function up()
     {
+        $faker = Faker::create();
+
         Schema::create('registrations', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
@@ -23,6 +27,17 @@ class RegistrationsCreate extends Migration
             
             $table->foreign('event_id')->references('id')->on('events')->onDelete('cascade');
         });
+
+        for ($i = 1; $i <= 30; $i++) {
+            DB::table('registrations')->insert([
+                'name' => $faker->name,
+                'email' => $faker->unique()->safeEmail,
+                'cpf' => mt_rand(10000000000, 99999999999),
+                'event_id' => mt_rand(1, 5), 
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]);
+        }
     }
 
     /**
