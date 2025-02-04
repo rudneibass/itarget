d522fe260a797ff32f914346bc7ca09a7c9bb737
rudnei <rudnei.nascimento@itarget.com.br>
2025-02-04 11:06:35 -0300

    feat/add-event-entity

---
+++ backend/app/Modules/Event/Domain/Entities/Event/EventDto.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Modules\Event\Domain\Entities\Event;
+
+use App\Modules\Event\Domain\Base\DtoBase;
+
+class EventDto extends DtoBase {
+    public ?string $id = null;
+    public ?string $eventId = null;
+    public ?string $registrationId = null;
+    public ?string $name = null;
+    public ?string $email = null;
+    public ?string $cpf = null;
+    public ?bool $published = null;
+
+
+    /*
+    public ?string $id = null;
+    public string $name;
+    public string $status;
+    public string $startDate;
+    public string $endDate;
+    */
+}
