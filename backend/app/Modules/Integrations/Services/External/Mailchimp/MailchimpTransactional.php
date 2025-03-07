851fad15afd67ac4b150c02dcdeeedb73f8a2f17
rudnei <rudnei.nascimento@itarget.com.br>
2025-03-07 12:49:24 -0300

    feat/test-mailchimp

--- backend/app/Modules/Integrations/Services/External/Mailchimp/MailchimpTransactional.php
+++ backend/app/Modules/Integrations/Services/External/Mailchimp/MailchimpTransactional.php
@@ -1,12 +1,25 @@
 <?php
 namespace App\Modules\Integrations\Services\External\Mailchimp;
 
+use Exception;
 use  MailchimpTransactional\ApiClient;
 
 class MailchimpTransactional {
-    public function __construct(){
+    /*public function __construct(){
         $mailchimp = new ApiClient();
         $mailchimp->setApiKey('0b71257616fe57536bb440323fb9d15b-us19');
         $mailchimp->users->ping();
+    }*/
+
+    public function __construct()
+    {
+        try {
+            $mailchimp = new ApiClient();
+            $mailchimp->setApiKey('0b71257616fe57536bb440323fb9d15b-us19');
+            $response = $mailchimp->users->ping();
+            print_r($response);
+          } catch (Exception $e) {
+                echo 'Error: ',  $e->getMessage(), "\n";
+          }
     }
 }
