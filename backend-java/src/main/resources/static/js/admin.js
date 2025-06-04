89c6d400bd43df324d312ffc7f8469f8bca7cff5
rudnei <rudnei.nascimento@itarget.com.br>
2025-06-04 17:45:18 -0300

    fix/layout

--- backend-java/src/main/resources/static/js/admin.js
+++ backend-java/src/main/resources/static/js/admin.js
@@ -9,12 +9,14 @@ const admin = {
           const scripts = tempDiv.querySelectorAll('script');
           container.innerHTML = '';
           container.append(...tempDiv.childNodes);
+          
           if (id !== null) {
             const inputId = container.querySelector('input#id');
             if (inputId) {
               inputId.value = id;
             }
           }
+
           scripts.forEach(script => {
             const newScript = document.createElement('script');
             if (script.type) newScript.type = script.type;
@@ -28,9 +30,10 @@ const admin = {
         });
     },
 
-    addTab: ({ id, title, url }) => {
-      const tabId = `tab-${id}`;
-      const contentId = tabId + '-content';
+    addTab: ({ tabId, title, url, id = null}) => {
+      const tabElementId = `tab-${tabId}`;
+      const contentId = tabElementId + '-content';
+
       // Já existe? Apenas ativa
       if (document.getElementById(contentId)) {
         $('.nav-link').removeClass('active');
@@ -39,40 +42,45 @@ const admin = {
         $(`#${contentId}`).addClass('active');
         return;
       }
+
       // Criar aba
       const newTab = $(`
         <li class="nav-item">
           <a class="nav-link" data-toggle="tab" href="#${contentId}">
             ${title}
-            <span style="cursor: pointer; margin-left: 5px;" onclick="admin.closeTab('${id}')">&times;</span>
+            <span style="cursor: pointer; margin-left: 5px;" onclick="admin.closeTab('${tabId}')">&times;</span>
           </a>
         </li>
       `);
+
       // Criar conteúdo
       const newContent = $(`
         <div class="tab-pane p-2" id="${contentId}">
-            <div class="tab-container" id="${tabId}-container" >
-              Carregando...
-            </div>
+          <div class="tab-container" id="${tabElementId}-container">
+            Carregando...
+          </div>
         </div>
       `);
+
       // Remover active de outras abas
       $('#tabs .nav-link').removeClass('active');
       $('#tab-content .tab-pane').removeClass('active');
+
       // Adicionar nova aba e conteúdo
       $('#tabs').append(newTab);
       $('#tab-content').append(newContent);
+
       // Ativar nova aba
       newTab.find('a').addClass('active');
       newContent.addClass('active');
+
       // Carregar conteúdo via fetch
       admin.navigate({
         url: url,
-        containerId: `${tabId}-container`,
+        containerId: `${tabElementId}-container`,
         id: id
       });
     },
-
     closeTab: (id) => {
         const tabId = `tab-${id}`;
         const contentId = tabId + '-content';
@@ -87,9 +95,9 @@ const admin = {
 
         // Se era ativa, ativa a primeira restante
         if (isActive) {
-            const $first = $('#tabs .nav-link').first();
-            $first.addClass('active');
-            $($first.attr('href')).addClass('active');
+          const $first = $('#tabs .nav-link').first();
+          $first.addClass('active');
+          $($first.attr('href')).addClass('active');
         }
     }
 };
