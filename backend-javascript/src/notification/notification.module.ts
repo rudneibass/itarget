ed007110877ac471dfa6dcbf64fc8102160875ea
rudnei <rudnei.nascimento@itarget.com.br>
2025-08-21 15:04:32 -0300

    refatct

--- backend-javascript/src/notification/notification.module.ts
+++ backend-javascript/src/notification/notification.module.ts
@@ -4,10 +4,21 @@ import { FindNotificationController } from './infra/controllers/find/find.notifi
 import { ProducerNotificationService } from './domain/services/producer/producer.notification.service';
 import { ConsumerNotificationService } from './domain/services/consumer/consumer.notification.service';
 import { StoreNotificationService } from './domain/services/store/store.notification.service';
+import { CreateNotificationRepository } from './domain/repositories/notification/database/create/create.notification.repository';
+import { DatabaseAdapter } from './infra/adapters/database/database.adapter';
 
 @Module({
   controllers: [CreateNotificationController, FindNotificationController],
-  providers: [ProducerNotificationService, ConsumerNotificationService, StoreNotificationService],
+  providers: [
+    ProducerNotificationService, 
+    ConsumerNotificationService, 
+    CreateNotificationRepository,
+    {
+      provide: 'IDatabaseAdapter',
+      useClass: DatabaseAdapter,
+    },
+    StoreNotificationService
+  ],
   exports: [],
 })
 export class NotificationrModule {}
