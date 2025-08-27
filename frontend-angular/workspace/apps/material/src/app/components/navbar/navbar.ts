42cfc50424a2b10ebbe4c95b52e91e2a4b967888
rudnei <rudnei.nascimento@itarget.com.br>
2025-08-27 14:54:42 -0300

    feat/frontend-angular-monorepo

---
+++ frontend-angular/workspace/apps/material/src/app/components/navbar/navbar.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'material-navbar',
+  imports: [],
+  templateUrl: './navbar.html',
+  styleUrl: './navbar.css',
+})
+export class Navbar {}
---
+++ frontend-angular/workspace/apps/material/src/app/pages/dashboard/dashboard.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'material-dashboard',
+  imports: [],
+  templateUrl: './dashboard.html',
+  styleUrl: './dashboard.css',
+})
+export class Dashboard {}
